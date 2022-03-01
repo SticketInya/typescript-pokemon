6daaff9875bb323391d7c57151f606385d004043
Barna Burom <burombarna@gmail.com>
2022-03-01 17:48:44 +0100

    feat(*): oop/functional, language elements

--- index.ts
+++ index.ts
@@ -12,9 +12,53 @@ const charizard: Pokemon = {
   name: "Charizard",
   type: PokemonType.FIRE,
 };
+const blastoise: Pokemon = {
+  name: "B",
+  type: PokemonType.WATER,
+};
 
-const arena = new Arena(ash, charizard);
+const arena = new Arena(ash, charizard, blastoise);
+
+function sayMyName(champ: Champion): void {
+  console.log(`My name is: ${champ.name}`);
+}
 
 let number = 10;
 number += 2;
 console.log(number);
+
+sayMyName(ash);
+const isMyPokemonTheWinner = arena.wouldMyPokemonWinInAFight();
+
+// branch
+if (isMyPokemonTheWinner) {
+  console.log("happpynesfjasl;dfj");
+} else {
+  console.log("sad very sad");
+}
+// for
+for (let i = 0; i < 2; i++) {
+  console.log(i);
+}
+//foreach
+[1, 2, 3, 4, 5].forEach((num: number) => {
+  console.log(num);
+  // non return/breakable
+});
+// forof
+for (const num of [1, 2, 3, 4, 5]) {
+  // Sequential and can be broken with return;
+}
+//Map
+const duplicate = [1, 3, 4].map((number) => number * 2);
+//While
+let i = 0;
+while (i < 3) {
+  i++;
+}
+// Object Iteration
+Object.keys(console.log);
+Object.keys(charizard).forEach((key) => {
+  console.log(key);
+  console.log(ash.name);
+});
--- src/arena.ts
+++ src/arena.ts
@@ -1,8 +1,29 @@
 import { Champion, Pokemon } from "./types";
 
 export class Arena {
-  constructor(champion: Champion, pokemon: Pokemon) {}
   protected champion: Champion;
 
-  public nameFunction(input: string) {}
+  constructor(
+    champion: Champion,
+    private pokemon: Pokemon,
+    private enemyPokemon: Pokemon
+  ) {
+    this.champion = champion;
+  }
+
+  public nameFunction(input: string) {
+    console.log(this.champion.name);
+  }
+
+  public wouldMyPokemonWinInAFight(): boolean {
+    console.log(this.pokemon.name, this.enemyPokemon.name);
+    return true;
+  }
 }
+
+// function wouldMyPokemonWinInAFight(
+//   myPokemon: Pokemon,
+//   enemyPokemon: Pokemon
+// ): boolean {
+//   return true;
+// }
