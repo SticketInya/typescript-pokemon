54bb7098f1fe6c91e935977f11dba088ce578ed9
Patrik Jokhel <mr.booman32@gmail.com>
2022-03-03 13:19:23 +0100

    feat(index.ts): add battle simulator

--- index.ts
+++ index.ts
@@ -21,48 +21,70 @@ const venosaur: Pokemon = {
   type: [PokemonType.GRASS, PokemonType.POISON],
 };
 
-const arena = new Arena(ash, charizard, venosaur);
+const arena = new Arena(ash, venosaur, blastoise);
 
 function sayMyName(champ: Champion): void {
   console.log(`My name is: ${champ.name}`);
 }
 
-let number = 10;
-number += 2;
-console.log(number);
+// let number = 10;
+// number += 2;
+// console.log(number);
 
-sayMyName(ash);
-const isMyPokemonTheWinner = arena.wouldMyPokemonWinInAFight();
+// sayMyName(ash);
+// const isMyPokemonTheWinner = arena.wouldMyPokemonWinInAFight(blastoise, charizard);
+const numberOfMatches = 10;
+playNumberOfMatches(numberOfMatches, arena);
 
-// branch
-if (isMyPokemonTheWinner) {
-  console.log("happpynesfjasl;dfj");
-} else {
-  console.log("sad very sad");
+
+function playNumberOfMatches(numberOfMatches: number, arena: Arena):void{
+  let myPokemonWinCount = 0;
+  for(let i = 0; i< numberOfMatches; i++){
+    if(arena.wouldMyPokemonWinInAFight()){
+      myPokemonWinCount++;
+    }
+  }
+  const pokemonWinRate = (myPokemonWinCount/numberOfMatches)*100;
+  console.log(`Your Pokemon's winrate is ${pokemonWinRate}% !`);
 }
-// for
-for (let i = 0; i < 2; i++) {
-  console.log(i);
-}
-//foreach
-[1, 2, 3, 4, 5].forEach((num: number) => {
-  console.log(num);
-  // non return/breakable
-});
-// forof
-for (const num of [1, 2, 3, 4, 5]) {
-  // Sequential and can be broken with return;
-}
-//Map
-const duplicate = [1, 3, 4].map((number) => number * 2);
-//While
-let i = 0;
-while (i < 3) {
-  i++;
-}
-// Object Iteration
-Object.keys(console.log);
-Object.keys(charizard).forEach((key) => {
-  console.log(key);
-  console.log(ash.name);
-});
+
+
+// if(isMyPokemonTheWinner){
+//   console.log("You won!");
+// }
+// else {
+//   console.log("You lost!");
+// }
+
+// // branch
+// if (isMyPokemonTheWinner) {
+//   console.log("happpynesfjasl;dfj");
+// } else {
+//   console.log("sad very sad");
+// }
+// // for
+// for (let i = 0; i < 2; i++) {
+//   console.log(i);
+// }
+// //foreach
+// [1, 2, 3, 4, 5].forEach((num: number) => {
+//   console.log(num);
+//   // non return/breakable
+// });
+// // forof
+// for (const num of [1, 2, 3, 4, 5]) {
+//   // Sequential and can be broken with return;
+// }
+// //Map
+// const duplicate = [1, 3, 4].map((number) => number * 2);
+// //While
+// let i = 0;
+// while (i < 3) {
+//   i++;
+// }
+// // Object Iteration
+// Object.keys(console.log);
+// Object.keys(charizard).forEach((key) => {
+//   console.log(key);
+//   console.log(ash.name);
+// });
